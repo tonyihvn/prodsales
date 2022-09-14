827b852d4123f3f034ad7221ac7e4ba71a06ca2b
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2022-09-14 03:34:53 +0100

    Verifications of Email

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-// implements MustVerifyEmail
+//
+class User extends Authenticatable implements MustVerifyEmail
 
-class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
